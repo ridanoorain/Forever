06d2247ec98fba8699005fe8d8051c8d3a24f9cc
Rida Noorain <ridanoorain16@gmail.com>
2025-08-29 21:50:10 +0530

    nmn

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {port: 5174}
+  server: {port: 5174},
+    build: {
+    // Add this build configuration
+    rollupOptions: {
+      external: [], // Ensure this is empty to bundle all dependencies
+    }
+  },
+  optimizeDeps: {
+    include: ['react-router-dom'] // Explicitly include react-router-dom
+  }
 })
